d5da4ff6a44d58f820304ce932e683e824d2e3d5
Sergey Ugdyzhekov <sergey.ugdyzhekov@jetbrains.com>
2018-07-01 23:40:35 +0300

    Fix param of spot instance module

--- bastion_host.tf
+++ bastion_host.tf
@@ -2,7 +2,7 @@ module "host" {
   source = "github.com/jetbrains-infra/terraform-aws-spot-fleet"
   subnet_id = "${var.subnet_id}"
   ssh_key = "${var.ssh_key}"
-  security_group_ids = "${aws_security_group.to_bastion.id}"
+  security_group_ids = ["${aws_security_group.to_bastion.id}"]
   name = "BastionHost"
   public_ip = true
 }
